6c33420f3efbb212249fc6c2df5cb2fa344702b9
lamter <lamter.fu@gmail.com>
2017-08-11 00:16:22 +0800

    - change : 添加错误汇报

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import json
 from wash import Washer
+import traceback
 
 settingFile = 'conf/kwarg.json'
 loggingConfigFile = 'conf/logconfig.json'
+serverChanFile = 'conf/serverChan.json'
+
 if __debug__:
     settingFile = 'tmp/kwarg.json'
     loggingConfigFile = 'tmp/logconfig.json'
 
+with open(serverChanFile, 'r') as f:
+    url = json.load(f)['serverChanUrl']
 
 with open(settingFile, 'r') as f:
     kwargs = json.load(f)
@@ -14,5 +19,20 @@ with open(settingFile, 'r') as f:
 with open(loggingConfigFile, 'r') as f:
     loggingConfig = json.load(f)
 
-w = Washer(loggingConfig=loggingConfig, **kwargs)
-w.start()
+try:
+    w = Washer(loggingConfig=loggingConfig, **kwargs)
+    w.start()
+except:
+    e = traceback.format_exc()
+    print(e)
+    import requests
+    import time
+    serverChanUrl = requests.get(url).text
+    text = 'washbar - {} - 数据清洗异常'.format(kwargs['mongoConf']['mongoLocal']['host'])
+    url = "{}{text}&desp={desp}".format(serverChanUrl, text=text, desp=e)
+    while True:
+        r = requests.get(url)
+        if r.status_code == 200:
+            break
+        else:
+            time.sleep(60)
